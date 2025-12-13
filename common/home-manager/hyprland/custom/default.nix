3d92d2220782eacfd3922e2b767fa2726933e5be
izen67 <17254913+izen67@users.noreply.github.com>
2025-12-13 17:20:40 +0200

    Add files via upload

---
+++ common/home-manager/hyprland/custom/default.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./waybar/waybar.nix
+    ./wlogout/wlogout.nix
+    ./hyprsunset.nix
+    ./fuzzel.nix
+    ./rofi.nix
+    ./mako.nix
+    ./swaylock.nix
+    ./swww.nix
+  ];
+}
