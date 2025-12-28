d826284ad2106091a30ebb9b89c36ee4f847f014
Munu <markrechy@gmail.com>
2025-12-29 01:07:39 +0200

    Update wallpaper IDs for HDMI and DP monitors

--- common/home-manager/wallpaperengine/linux-wallpaperengine.nix
+++ common/home-manager/wallpaperengine/linux-wallpaperengine.nix
@@ -12,7 +12,7 @@
         monitor = "HDMI-A-1";
 
         wallpaperId =
-          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3600653235";
+          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3538825305";
 
         fps = 60;
 
@@ -27,7 +27,7 @@
         monitor = "DP-1";
 
         wallpaperId =
-          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3538825305";
+          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3382111183";
       }
     ];
   };
