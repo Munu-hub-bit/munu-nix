bd638d7e6c084c71e2fc69a1fd32e6c9be2b2495
Munu <markrechy@gmail.com>
2025-12-29 01:31:03 +0200

    Update linux-wallpaperengine.nix

--- common/home-manager/wallpaperengine/linux-wallpaperengine.nix
+++ common/home-manager/wallpaperengine/linux-wallpaperengine.nix
@@ -27,7 +27,7 @@
         monitor = "DP-1";
 
         wallpaperId =
-          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3445942378";
+          "/mnt/extras/SteamLibrary/steamapps/workshop/content/431960/3536608741";
       }
     ];
   };
